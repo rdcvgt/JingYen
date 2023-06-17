6516a85eda202661bd8327321b7a49062f67026c
finleypeng <weilllyox1020@gmail.com>
2023-06-17 17:31:12 +0800

    feat: add the basic layout of footer to public pages

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import styled from "@emotion/styled";
+import Link from "next/link";
+import { theme } from "@/app/globalStyles";
+
+const Container = styled.div`
+	background-color: ${theme.color.green[100]};
+	position: relative;
+	bottom: 0;
+	left: 0;
+	height: 120px;
+	width: 100%;
+	margin-top: 20px;
+`;
+
+const Info = styled.div`
+	width: 1120px;
+	height: 100%;
+	margin: 0 auto;
+	display: flex;
+	align-items: center;
+`;
+
+const TopArea = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+`;
+
+const LeftArea = styled.div``;
+
+const RightArea = styled.div`
+	color: ${theme.color.beige[60]};
+	font-size: 9px;
+`;
+
+const Item = styled.div``;
+
+const Logo = styled.div`
+	color: ${theme.color.beige[60]};
+`;
+
+export default function Footer() {
+	return (
+		<Container>
+			<Info>
+				<TopArea>
+					<LeftArea>
+						<Logo>瑾諺工程</Logo>
+					</LeftArea>
+					<RightArea>
+						<Item>Copyright - 瑾諺工程</Item>
+						<Item>
+							<Link href="/login">Content Management System</Link>
+						</Item>
+					</RightArea>
+				</TopArea>
+			</Info>
+		</Container>
+	);
+}
